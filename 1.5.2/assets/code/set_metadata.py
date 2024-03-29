75483adc000a2b73ecce98c2574ca0045137aed4
rafaellichen <rafaellichen@gmail.com>
2024-03-29 13:07:51 -0800

    Removed everything with mike 2.0.0

--- 1.5.2/assets/code/set_metadata.py
+++
@@ -1,6 +0,0 @@
-from sentence_transformers import SentenceTransformer
-
-model = SentenceTransformer('all-MiniLM-L6-v2')
-# set metadata generation function for service object demo
-demo.set_metadata("bert-embedding",
-                  lambda x: list(model.encode(x).astype(float)), 500)
